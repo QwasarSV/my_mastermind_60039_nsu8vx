e0cd599d52bfb434df997c659c67bada1c2b370a
quinton mincy <mincy_q@qwasar.io>
2022-07-19 17:54:35 +0000

    mastermind... working on infinite loops

--- my_mastermind.c
+++ my_mastermind.c
@@ -10,46 +10,18 @@
 
 int my_mastermind(char secret_code[]){
 
-bool valid_input = false;//will be made true if the user input is valid
 char valid_code[4];//the user input will be transcribed into this variable
-while (valid_input == false) {
-    // get user input
-    char usr_input[10];
-    read(0, usr_input, 10);
+char usr_input[4];
+int nbr_read;
 
-    // validate input
-    int length = strlen(usr_input);//gets number of characters in the string
-    if (length != 8) {/*if number of characters is not 4 (valid secret code), read(0, input, 10)
-                        is called again, and will be called until a valid input is entered.
-                        ...further checks are implemented below to determine if each of the 4 characters
-                        are numbers (between ascii values 48 and 55-- digits 0-7)*/            
+while((nbr_read = read(0, usr_input, 4)) != 4){
         printf("Wrong input!\n");
-        continue;
-    }
-    /*if each of the 4 input characters are string versions of the digits 0-7, then the variable
-        valid input will be made true, halting the execution of the while loop
-        ...after while loop is terminated, the valid input (guess of the secret code) will be fed into the my_mastermind
-            function to determine whether the guess matches the actual secret code*/
-    if  ( 
-           (usr_input[0] >= 48 && usr_input[0] <= 55) 
-        && (usr_input[1] >= 48 && usr_input[1] <= 55)
-        && (usr_input[2] >= 48 && usr_input[2] <= 55) 
-        && (usr_input[3] >= 48 && usr_input[3] <= 55)
-        )
-        {
-        valid_input = true; //if above conditions are true, valid input will be true
-        //transfers usr_input to variable outside of while loop, which will be compared to actul secret code
-        for(int i = 0;i < CODE_LENGTH;i++){
-            valid_code[i] = usr_input[i];
-        }
-        }
-    else{
-        printf("Wrong input!\n"); /*if one or more conditions  is false, "Wrong Inpiut!
-                                    will be printed to the standard output, and the read() function
-                                    will be called again until valid input is entered into terminal"*/
-    }
 }
 
+//transfers usr_input to variable outside of while loop, which will be compared to actul secret code
+for(int i = 0;i < CODE_LENGTH;i++){
+    valid_code[i] = usr_input[i];
+}
 
 int well_n_miss_place[2] = {};//will return the number of well placed and missplaced pieces of the code guessed by user
 

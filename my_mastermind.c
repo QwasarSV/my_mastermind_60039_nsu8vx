7a5026afb91706f12c8b33c5ac9139fe2090171a
quinton mincy <mincy_q@qwasar.io>
2022-07-21 20:54:26 +0000

    my_mastermind final draft

--- my_mastermind.c
+++ my_mastermind.c
@@ -1,12 +1,4 @@
-#define CODE_LENGTH 4
-#define RANDER 7
-
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <stdbool.h>
-#include <time.h>
+#include "my_mastermind.h"
 
 int my_mastermind(char secret_code[]){
 
@@ -15,11 +7,11 @@ char valid_code[4];//the user input will be transcribed into this
 while (valid_input == false) {
     // get user input
     char usr_input[10];
-    read(0, usr_input, 10);
+    int n_bytes = read(0, usr_input, 10);
     // validate input
-    int length = strlen(usr_input);//gets number of characters in the string
-    if (length != 8) {/*if number of characters is not 4 (valid secret code), read(0, input, 10)
+    if (n_bytes != CODE_LENGTH + 1) {/*if number of characters is not 4 (valid secret code), read(0, input, 10)
                         is called again, and will be called until a valid input is entered.
+                        ...n_bytes is 5 because of?? null terminator, enter key??
                         ...further checks are implemented below to determine if each of the 4 characters
                         are numbers (between ascii values 48 and 55-- digits 0-7)*/            
         printf("Wrong input!\n");
@@ -73,64 +65,4 @@ if(well_n_miss_place[0] == CODE_LENGTH){//returns EXIT_SUCCESS if the all the pi
 
 printf("Well placed pieces: %d\nMisplaced pieces: %d\n---\n",well_n_miss_place[0],well_n_miss_place[1]);
 return EXIT_FAILURE;
-}
-///////////////////////////////////////////////////////////////////////////////////////////////////
-
-int main(int argc, char **argv){
-    char secret_code[4] = {};//will hold the secret code in an array of integers
-    int max_attempts = 10;
-    bool c_flag = false;//will be used to determine whether program needs to produce a random secret code***
-    srand(time(NULL));//time() produces, in seconds, the time since january 1, 1970, providing a unique seed value for rand() each time program is called
-    if(argc == 1)//if no entry is given, random secret code is created with rand(),and max attempts = 10
-    {
-        
-        for(int i = 0;i < CODE_LENGTH;i++){
-            int random_number = rand() % RANDER;//RAND_MAX is already defined
-            sprintf(&secret_code[i],"%c",random_number);//turns random numbers into characters
-                                                        //...need to fix formatting later
-        }
-    }
-    else
-    {
-    for(int i = 1;i < argc;i++){//will determine what flags (-c, -t, or both)...
-        if(argv[i][0] == '-'){//detects that a flag is present... the next character after hyphen will be c or t
-            char flag = argv[i][1];//holds either a c or t
-            switch(flag){
-            //switch statement will trigger the action of one of two cases... a c or a t flag
-            case 'c':
-                for(int j = 0;j < CODE_LENGTH;j++){
-                    secret_code[j] = argv[i + 1][j];//transcribes the secret code provided in command line to a string array (each character representing an integer 0-7)
-                    c_flag = true;//will stop the if statement below the loop from creating a random value for the secret code
-                }
-                break;
-
-            case 't':
-                max_attempts = atoi(argv[i + 1]);//i is the flag (t in this case), so i + 1 represents the next CLA, the specified max attempts
-                                                //...i + 1 would represent the specified secret code in the context of the c flag in the above case
-                break;
-            }
-        }
-    }
-    if(c_flag == false){//***if there was only a -t flag, then a random secret code will be genreated
-        for(int i = 0;i < CODE_LENGTH;i++){
-            int random_number = rand() % RANDER;//RAND_MAX is already defined
-            sprintf(&secret_code[i],"%c",random_number);//turns random numbers into characters
-        }
-    }
-    }
-    printf("Will you find the secret code?\nPlease enter a valid guess\n---\n");//opening line to game
-
-    for(int i = 1;i < max_attempts;i++){/*will call the my_mastermind function a max of (max_attempts) times -default is 10
-                                          ...will print the return value of my_mastermind funciton--either the number of well/missplaced
-                                             pieces of the code
-                                          ...if the code is guessed correctly, will print "Congratz! You did it!" and end the game*/
-        
-        printf("Round %d\n",i);
-        int result = my_mastermind(secret_code);
-        if(result == EXIT_SUCCESS){//all pieces match
-            printf("Congratz! You did it!\n");
-            return EXIT_SUCCESS;
-        }
-    }
-return 0;
 }
